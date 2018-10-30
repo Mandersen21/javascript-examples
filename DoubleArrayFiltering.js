f84c39e3857050ab6ca7b8a8e8975876bcfa1ff9
mandersen21 <mikkelandersen21@gmail.com>
2018-10-30 20:01:39 +0100

    added file

---
+++ DoubleArrayFiltering.js
@@ -0,0 +1,15 @@
+
+let array_1 = [{ id: '_i23455szq', description: 'description text' }, { id: '_h6yf2uk1n', description: 'description text' }]
+
+let array_2 = [
+    { id: '_h6yf2uk1n', description: 'text', capacity: 2 },
+    { id: '_465324423', description: 'text', capacity: 4 },
+    { id: '_i23455szq', description: 'text', capacity: 0 },
+    { id: '_54234gfgd', description: 'text', capacity: 0 },
+]
+
+const ids = new Set(array_1.map(({ id, description }) => id));
+console.log(ids)
+
+let result = array_2.filter(({ id, capacity }) => ids.has(id) || capacity > 0)
+console.log(result);
